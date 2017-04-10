e9f180b7b45363fb34ebae3f505696400ee78e47
多才 <enmoton@vip.qq.com>
2017-04-10 18:28:44 +0800

    添加 NLog 记录日志到文件

--- LoggingDemo/Controllers/HomeController.cs
+++ LoggingDemo/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace LoggingDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
         {
-            return Content("Index");
+			_logger.LogInformation("你访问了首页");
+			_logger.LogWarning("警告信息");
+			_logger.LogError("错误信息");
+			return Content("Index");
         }
     }
 }
--- LoggingDemo/Startup.cs
+++ LoggingDemo/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using NLog.Extensions.Logging;
 
 namespace LoggingDemo
 {
@@ -34,10 +36,10 @@ namespace LoggingDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+			loggerFactory.AddNLog();//添加NLog
 
-            if (env.IsDevelopment())
+			if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
