1aa29c1b797cdfb74ba230dbfce89cd9c542abfd
多才 <enmoton@vip.qq.com>
2017-04-12 13:20:11 +0800

    添加 中间组件Demo

---
+++ MiddlewareDemo/Middleware/HelloworldMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo.Middleware
+{
+	public class HelloworldMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public HelloworldMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			await context.Response.WriteAsync("Hello World Middleware!");
+			await _next(context);
+		}
+	}
+
+	public static class HelloworldMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseHelloworldMiddleware(this IApplicationBuilder app)
+		{
+			app.UseMiddleware<HelloworldMiddleware>();
+			return app;
+		}
+	}
+}
--- MiddlewareDemo/Startup.cs
+++ MiddlewareDemo/Startup.cs
@@ -7,37 +7,86 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using MiddlewareDemo.Middleware;
 
 namespace MiddlewareDemo
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
 
-        public IConfigurationRoot Configuration { get; }
+		public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Add framework services.
+			services.AddMvc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
 
-            app.UseMvc();
-        }
-    }
+			app.UseMvc();
+
+			// 添加自定义中间件
+			app.Map("/test", MapTest);
+
+			// 添加自定义中间件
+			// /*?a
+			app.MapWhen(context =>
+			{
+				return context.Request.Query.ContainsKey("a");
+			}, a =>
+			{
+				a.Run(async context =>
+				{
+					await context.Response.WriteAsync("Hello World MapWhen!");
+				});
+			});
+
+			// 添加自定义中间件
+			app.Use(async (context, next) =>
+			{
+				await context.Response.WriteAsync("Hello World Use !");
+				await next();
+			});
+
+			// 添加自定义中间件
+			app.UseHelloworldMiddleware();
+
+			// 添加自定义中间件
+			app.Run(async context =>
+			{
+				await context.Response.WriteAsync("Hello World Run!");
+			});
+		}
+
+		private void MapTest(IApplicationBuilder app)
+		{
+			// /test/level
+			app.Map("/level", a =>
+			{
+				a.Run(async context =>
+				{
+					await context.Response.WriteAsync("Hello World Run!");
+				});
+			}).Run(async context =>
+			{
+				await context.Response.WriteAsync("Url is " + context.Request.PathBase + context.Request.Path);
+			});
+		}
+	}
 }
