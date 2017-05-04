19bab45df182f451bbc00edfd3ed8daaf0f1982b
多才 <enmoton@vip.qq.com>
2017-05-04 09:29:33 +0800

    添加邮件发送

--- Html解析/Program.cs
+++ Html解析/Program.cs
@@ -13,15 +13,15 @@ namespace Html解析
 
             //加载Web 的页面并解析内容
             HttpClient client = new HttpClient();
-            string html = client.GetStringAsync("http://www.gongjuji.net").Result;
+            string html = client.GetStringAsync("https://book.douban.com/subject_search?search_text=%E6%B7%B1%E5%BA%A6%E5%AD%A6%E4%B9%A0").Result;
 
             //HTML 解析成 IDocument
             var dom = new HtmlParser().Parse(html);
             //从dom中提取所有class='col-sm-6'的div标签
-            var listDiv = dom.QuerySelectorAll("div.col-sm-6");
+            var listDiv = dom.QuerySelectorAll(".info");
             foreach (var item in listDiv)
             {
-                Console.WriteLine(item.QuerySelector("h3").InnerHtml);
+                Console.WriteLine(item.QuerySelector("a").GetAttribute("href"));
             }
         }
     }
---
+++ SendMail/Program.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+
+namespace SendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendEmailAsync("524246918@qq.com", "主题", "消息").Wait();
+        }
+
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("管理员", "admin@suqingfa.win"));
+            emailMessage.To.Add(new MailboxAddress("mail", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtpdm.aliyun.com", 25);
+                await client.AuthenticateAsync("admin@suqingfa.win", "eXfZIsYJX23mN6Hp");
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
