04c2ea8ee3361947df9d3c88bc26681d3e09d06b
多才 <enmoton@vip.qq.com>
2017-05-06 23:26:54 +0800

    Add Project RunAsService

---
+++ RunAsWindowsService/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using System.IO;
+
+namespace WebRunAsWindowsService
+{
+    public class Startup
+    {
+        /// <summary>
+        /// sc create MyService binPath="E:\CoreLearn\WebRunAsWindowsService\bin\Debug\net461\win10-x64\WebRunAsWindowsService.exe"
+        /// sc delete MyService
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            bool isService = true;
+            if (Debugger.IsAttached || args.Contains("--console"))
+            {
+                isService = false;
+            }
+
+            var pathToContentRoot = Directory.GetCurrentDirectory();
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(pathToContentRoot)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
