eebd593bfa7dcee3fb4a42df95b74d3594cdab3e
多才 <enmoton@vip.qq.com>
2017-05-07 18:19:59 +0800

    Add use di in concole

---
+++ UseDiInConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace UseDiInConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new ServiceCollection()
+                .AddTransient<IService, Service>()
+                .AddTransient<Program>()
+                .BuildServiceProvider();
+
+            var service = provider.GetService<Program>();
+        }
+
+        public Program(IService s)
+        {
+            s.Test();
+            fu();
+
+            int fu() => 10;
+        }
+    }
+
+    public interface IService
+    {
+        void Test();
+    }
+
+    public class Service : IService
+    {
+        public void Test() => Console.WriteLine("Test Method");
+    }
+}
