e17efebb87320805cc28bc14b88d184cadc9f61d
多才 <enmoton@vip.qq.com>
2017-05-07 15:34:17 +0800

    add project Localization

---
+++ NetCoreLocalization/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+
+namespace NetCoreLocalization
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IStringLocalizer<Startup> localizer)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("zh"),
+                SupportedCultures = new[]
+                {
+                    new CultureInfo("zh"),
+                    new CultureInfo("en")
+                },
+                SupportedUICultures = new[]
+                {
+                    new CultureInfo("zh"),
+                    new CultureInfo("en")
+                }
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Hello World!{localizer["name"]}");
+            });
+        }
+    }
+}
